b1c37d0e184f1c7cd7aa21622beb2e012b4fe42d
Paul Daniel <paulgdan@gmail.com>
2014-11-03 19:06:04 -0800

    changed doc

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -41,7 +41,7 @@ class FileUploader < CarrierWave::Uploader::Base
     end
 
     def image?(file)
-      File.extname(file.filename.to_s).match(/jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF/)
+      File.extname(file.document_filename.to_s).match(/jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF/)
     end
 
 end
